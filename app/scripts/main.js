432920eeb40da0e6a32df9869fd3b2bfe1db74eb
Lance Batson <lancebatsondev@gmail.com>
2014-09-15 19:54:19 +0000

    timeline chart and period selection with random data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,55 @@
-console.log('\'Allo \'Allo!');
+var x = ['x'],
+    minutes = ['Minutes'],
+    PERIOD = 7,
+    chart;
+
+$(function() {
+  // listener to update period and data on selection
+  $('#period').on('change', function() {
+    var selection = $(this).val();
+    if (selection !== PERIOD) {
+      PERIOD = $(this).val();
+      update();
+    }
+  });
+  // get initial data for chart with default settings
+  loadData();
+  // render chart
+  chart = c3.generate({
+    data: {
+      x: 'x',
+      type: 'bar',
+      columns: [x, minutes]
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y-%m-%d'
+        }
+      }
+    }
+  });
+});
+
+function loadData() {
+  // reset arrays to first values
+  x = [x[0]]
+  minutes = [minutes[0]];
+  // set random data from yesterday to end of period
+  for (var i = 0; i < PERIOD; i++) {
+    minutes.push(Math.floor(Math.random() * 100));
+    var date = new Date();
+    date.setDate(date.getDate() + (i - PERIOD));
+    var dateString = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
+    x.push(dateString);
+  }
+}
+
+function update() {
+  // reload data and chart
+  loadData();
+  chart.load({
+    columns: [x, minutes]
+  })
+}
