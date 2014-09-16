baf62ee771f27a5a1f1bbb6c1e4467880b25a8b2
Lance Batson <lancebatsondev@gmail.com>
2014-09-16 17:37:27 +0000

    use Parse Event class data to populate chart

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,8 @@ var appId = '8ZRnPK6TTi8p9uGrARtm0RvF1eAKgzciL18oASuF',
     key = 'bhXp8UST70ApUILSCvE2llMLTh5RBVRgP2K6Jl4a',
     x = ['x'],
     minutes = ['Minutes'],
-    PERIOD = 7,
+    PERIOD = 1,
+    events = [],
     chart;
 
 $(function() {
@@ -10,20 +11,16 @@ $(function() {
   // listener to update period and data on selection
   $('#period').on('change', function () {
     var selection = $(this).val();
-    if (selection !== PERIOD) {
-      PERIOD = $(this).val();
+    if (PERIOD !== selection) {
+      PERIOD = selection;
       update();
     }
   });
-  // get temp data for chart
+  // get data for chart
   loadData()
-  .then(function (data) {
-    console.log(_.groupBy(data, function (event) {
-      return moment(event.attributes.enterTime).format('YYYY-M-D');
-    }));
-    return loadData(true);
-  })
   .done(function (data) {
+    events = data;
+    organizeData();
     $('.loading-spinner').hide();
     $('.main').show();
     // render chart
@@ -45,42 +42,60 @@ $(function() {
   });
 });
 
-function loadData(temp) {
+function loadData() {
   var deferred = $.Deferred();
-  if (temp) {
-    // reset arrays to first values
-    x = [x[0]]
-    minutes = [minutes[0]];
-    // set random data from yesterday to end of period
-    for (var i = 0; i < PERIOD; i++) {
-      minutes.push(Math.floor(Math.random() * 100));
-      x.push(moment().subtract('days', i + 1).format('YYYY-M-D'));
+  // get events from Parse based on period selection
+  var Event = Parse.Object.extend("Event");
+  var query = new Parse.Query(Event);
+  console.log(new Date(moment().format()));
+  query.lessThanOrEqualTo('exitTime', new Date(moment().format()))
+  query.greaterThanOrEqualTo('enterTime', new Date(moment().subtract(PERIOD - 1, 'days').startOf('day').format()));
+  query.find({
+    success: function (results) {
+      deferred.resolve(results);
+    },
+    error: function (error) {
+      alert("Error: " + error.code + " " + error.message);
+      deferred.resolve();
     }
-    deferred.resolve();
-  } else {
-    var Event = Parse.Object.extend("Event");
-    var query = new Parse.Query(Event);
-    query.find({
-      success: function (results) {
-        deferred.resolve(results);
-      },
-      error: function (error) {
-        alert("Error: " + error.code + " " + error.message);
-        deferred.resolve();
-      }
-    });
-  }
+  });
   return deferred;
 }
 
+function organizeData() {
+  // group events by date
+  var eventGroups = _.groupBy(events, function (event) {
+    return moment(event.attributes.enterTime).format('YYYY-M-D');
+  });
+  // add dates
+  x = x.concat(_.keys(eventGroups));
+  _.each(eventGroups, function (events) {
+    // sum difference between enter and exit times for each date
+    var time = 0;
+    _.each(events, function (event) {
+      var start = moment(event.attributes.enterTime);
+      var exit = (event.attributes.exitTime) ? event.attributes.exitTime : moment();
+      var end = moment(exit);
+      time += end.diff(start, 'minutes');
+    });
+    // add minutes for date
+    minutes.push(time);
+  });
+}
+
 function update() {
-  // reload data and chart
+  // reset arrays to first values
+  x = x.slice(0,1);
+  minutes = minutes.slice(0,1);
+  var spinner = $('#loading-spinner');
+  spinner.show();
   loadData()
-  .then(function (data) {
-    console.log(data);
-    return loadData(true);
-  })
-  .done(function () {
+  .done(function (data) {
+    events = null;
+    events = data;
+    organizeData();
+    spinner.hide();
+    // reload data and chart
     chart.load({
       columns: [x, minutes]
     });
